97bfa00df40dfee8f0503571b88bc3c495f914cf
AlexUnderGroundPro <76145433+AlexUnderGroundPro@users.noreply.github.com>
2021-07-09 00:44:55 +0300

    PreFinal

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Mover : MonoBehaviour
 {
     public Transform SelfTransform;
     private Vector3 force;
     private bool isAccelerated;
+    public Tilemap Map;
+    public TileBase GroundTile;
 
     public void LeftRotation()
     {
@@ -19,7 +22,7 @@ public class Mover : MonoBehaviour
     public void Acceleration()
     {
         isAccelerated = true;
-        force += (SelfTransform.up * Time.deltaTime) * 0.1f;
+        force += (SelfTransform.up * Time.deltaTime) * 0.06f;
     }
     
     public void LateUpdate()
@@ -28,7 +31,16 @@ public class Mover : MonoBehaviour
         {
             force = Vector3.Lerp(force, Vector3.zero, Time.deltaTime);
         }
+        if( GroundTile == Map.GetTile(new Vector3Int((int)SelfTransform.position.x,
+            (int)SelfTransform.position.y,
+            (int)SelfTransform.position.z)))
+        {
+           force *= 0.9f;
+        }
+        
         SelfTransform.position += force;
+        
+        
         isAccelerated = false;
 
     }
